af9d4a5a017fff719c56e7934e6e0fb5c050c454
Marc Mac <marc@flerin.com>
2013-07-11 11:45:51 +0100

    remove the angular controller code so we just have empty controllers and failing tests for the demo

--- app/public/js/user/user.js
+++ app/public/js/user/user.js
@@ -2,72 +2,14 @@ angular.module('user', ['ngResource', 'toastr'])
 
     .factory('userApi',function ($resource) {
         var resource = $resource('/api/:action/:id/:searchTerm', {action: 'user', id: '@_id'},
-            {
-                update: {method: 'PUT'}
+                                { update: {method: 'PUT'}
             });
 
         return resource;
     }).controller('UserListCtrl',function ($scope, $location, userApi, message) {
-        $scope.model = {
-            message: 'Here are my users'
-        };
 
-      $scope.users =  userApi.query();
-
-        $scope.actions = {
-            delete: function (user) {
-                userApi.delete({id: user._id}, function () {
-                    var index = $scope.users.indexOf(user);
-                    if (index >= 0) {
-                        $scope.users.splice(index, 1);
-                        message.info('User ' + user.name + ' deleted');
-                    }
-                });
-            },
-
-            goToCreate: function () {
-                $location.path('/users/create');
-            }
-        }
     }).controller('UserEditCtrl',function ($scope, $routeParams, $location, userApi, message) {
-        var originalUser = {name: '', age: '', email: ''};
-        $scope.model = {
-            message: 'Edit Users'
-        };
 
-        var userId = $routeParams.id;
-
-        $scope.user = userApi.get({id: userId});
-
-        //Clear all form fields in the View
-        $scope.cancel = function () {
-            $scope.user = originalUser;
-        };
-
-        $scope.save = function (user) {
-            userApi.update(user, function () {
-                message.success('Success', 'User Updated.');
-                $location.path('users');
-            })
-        };
     }).controller('UserCreateCtrl', function ($scope, $location, userApi, message) {
-        var originalUser = {name: '', age: '', email: ''};
-        var user = angular.copy(originalUser);
 
-        $scope.user = user;
-
-        //Clear all form fields in the View
-        $scope.cancel = function () {
-            $scope.user = originalUser;
-        };
-
-        //Add save method to the scope
-        $scope.save = function (user) {
-            console.log('user is:', user);
-            userApi.save(user, function () {
-                message.success('Success', 'New User Created.');
-                $location.path('users');
-            });
-            $scope.cancel();
-        };
     });
